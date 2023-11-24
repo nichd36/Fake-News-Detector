1e4c9102d0fe725aea8c4202d1d5aca4e5c395eb
Nicholas Dylan <128449429+nichd36@users.noreply.github.com>
2023-11-24 16:52:41 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -31,18 +31,33 @@ def stemming(content):
 
 def check_reality(news):
         model = joblib.load('model_lemmatization_svm_linear.pkl')
+        model_lr = joblib.load('model.pkl')
+    
         news = stemming(news)
-        tfidf_vectorizer = joblib.load('tfidf_vectorizer_svm_lemma_linear.pkl')                                 
+    
+        tfidf_vectorizer = joblib.load('tfidf_vectorizer_svm_lemma_linear.pkl')   
+        tfidf_vectorizer_lr = joblib.load('tfidf_vectorizer.pkl')   
+    
         input_features = tfidf_vectorizer.transform([news])
         prediction = model.predict(input_features)
+        prediction_lr = model_lr.predict(input_features)
+    
         probs = model.predict_proba(input_features)
+        probs_lr = model_lr.predict_proba(input_features)
 
-        probability_real = probs[0][0]
-        probability_fake = probs[0][1]
+        probability_real = probs_lr[0][0]
+        probability_fake = probs_lr[0][1]
 
+        st.markdown("Results from model with SVM")
         if prediction[0] == 1:
+            st.write("SVM say the news is fake")
+        else:
+            st.write("SVM say the news is real")
+            
+        if prediction_lr[0] == 1:
                 pred = round(probability_fake*100)
                 if pred < 71:
+                        st.write("Model LR")
                         st.warning("Proceed with caution, as the certainty is low ⚠️")
                         st.markdown(
                         """
@@ -54,6 +69,7 @@ def check_reality(news):
                         unsafe_allow_html=True,
                         )
                 else:
+                        st.write("Model LR")
                         st.markdown(
                         """
                         <style>
